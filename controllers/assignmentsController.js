f46d533f2fc57ac26a0080f0772f2e31783a85ef
KOPLE <boldnessking26@gmail.com>
2025-06-16 10:47:24 +0700

    add file unlock  admin

--- controllers/assignmentsController.js
+++ controllers/assignmentsController.js
@@ -1,28 +1,42 @@
-const getDbConnection = require('../utils/dbManager');
-const assignmentSchema = require('../models/Assignment');
+const getDbConnection = require("../utils/dbManager");
+const assignmentSchema = require("../models/Assignment");
 
 exports.createAssignment = async (req, res) => {
   try {
-    const appId = req.headers['x-app-id'];
+    const appId = req.headers["x-app-id"];
     if (!appId) return res.status(400).json({ message: "Missing app-id" });
 
     const conn = await getDbConnection(appId);
-    const Assignment = conn.model('Assignment', assignmentSchema);
+    const Assignment = conn.model("Assignment", assignmentSchema);
 
-    const { complaintId, userId, solution, solutionImages, completedAt, note } = req.body;
+    const {
+      complaintId,
+      userId,
+      solution,
+      solutionImages,
+      completedAt,
+      note,
+      assignedAt,
+    } = req.body;
+
     const newAssignment = new Assignment({
       complaintId,
       userId,
-      assignedAt: new Date(),
+      assignedAt: assignedAt ? new Date(assignedAt) : new Date(),
       solution: Array.isArray(solution) ? solution : [solution],
       solutionImages: solutionImages || [],
       completedAt: completedAt || null,
-      note: note || '',
+      note: note || "",
     });
     await newAssignment.save();
-    res.status(201).json({ message: 'Assignment created successfully', assignment: newAssignment });
+    res
+      .status(201)
+      .json({
+        message: "Assignment created successfully",
+        assignment: newAssignment,
+      });
   } catch (error) {
-    console.error('Error creating assignment:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error creating assignment:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
